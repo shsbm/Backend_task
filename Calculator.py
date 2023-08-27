cc7c832bc91318d2379796603dbe40a91688efa3
Shashank Shubham <114795030+shsbm@users.noreply.github.com>
2023-08-27 12:28:13 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,46 +1,36 @@
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+app = Flask(_name_)
 
-operations_history = []
+operations_history = []  # To store the last 20 operations
 
-def perform_operation(operands, operators):
-    result = operands[0]
-    for i in range(len(operators)):
-        if operators[i] == "plus":
-            result += operands[i + 1]
-        elif operators[i] == "minus":
-            result -= operands[i + 1]
-        elif operators[i] == "into":
-            result *= operands[i + 1]
-    return result
+def calculate_expression(expression):
+    try:
+        result = eval(expression)
+        return result
+    except Exception as e:
+        return str(e)
 
 @app.route('/', methods=['GET'])
 def index():
     return "Welcome to the Calculator App!"
 
-@app.route('/calculate', methods=['GET'])
-def calculate():
+@app.route('/evaluate/<path:expression>', methods=['GET'])
+def evaluate(expression):
     try:
-        operation = request.args.get('operation')
-        parts = operation.split('/')
-        
-        operands = [int(parts[i]) for i in range(1, len(parts), 2)]
-        operators = [parts[i] for i in range(2, len(parts), 2)]
-        
-        result = perform_operation(operands, operators)
-        operations_history.append({'question': operation, 'answer': result})
+        result = calculate_expression(expression)
+        operations_history.append({'question': expression, 'answer': result})
         
         if len(operations_history) > 20:
-            operations_history.pop(0)
-        
-        return jsonify({'question': operation, 'answer': result})
+            operations_history.pop(0)  # Remove the oldest operation if history exceeds 20
+            
+        return jsonify({'question': expression, 'answer': result})
     except Exception as e:
         return jsonify({'error': str(e)})
 
 @app.route('/history', methods=['GET'])
 def history():
-    return jsonify({'history': operations_history})
+    return jsonify(operations_history)
 
-if __name__ == '__main__':
-    app.run()
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
