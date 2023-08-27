7a53b765b4aafac6e4324f57fef1533d8925ceb9
Shashank Shubham <114795030+shsbm@users.noreply.github.com>
2023-08-27 12:55:19 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,37 +1,65 @@
 from flask import Flask, request, jsonify
+import sqlite3
 
 app = Flask(_name_)
 
-operations_history = []  # To store the last 20 operations
+# Initialize SQLite database
+conn = sqlite3.connect('operations.db')
+cursor = conn.cursor()
 
-def calculate_expression(expression):
-    try:
-        expression = expression.replace('/plus/', '+').replace('/minus/', '-').replace('/into/', '*').replace('/by/', '/')
-        result = eval(expression)
-        return result
-    except Exception as e:
-        return str(e)
+# Create history table if not exists
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS history (
+        id INTEGER PRIMARY KEY,
+        operation TEXT,
+        result INTEGER
+    )
+''')
+conn.commit()
+
+def perform_operation(operands, operators):
+    result = operands[0]
+    for i in range(len(operators)):
+        if operators[i] == "plus":
+            result += operands[i + 1]
+        elif operators[i] == "minus":
+            result -= operands[i + 1]
+        elif operators[i] == "into":
+            result *= operands[i + 1]
+    return result
 
 @app.route('/', methods=['GET'])
 def index():
     return "Welcome to the Calculator App!"
 
-@app.route('/evaluate/<path:expression>', methods=['GET'])
-def evaluate(expression):
+@app.route('/evaluate/<path:operation>', methods=['GET'])
+def evaluate(operation):
     try:
-        result = calculate_expression(expression)
-        operations_history.append({'question': expression, 'answer': result})
+        parts = operation.split('/')
+        operands = [int(parts[i]) for i in range(0, len(parts), 2)]
+        operators = [parts[i] for i in range(1, len(parts), 2)]
+        result = perform_operation(operands, operators)
         
-        if len(operations_history) > 20:
-            operations_history.pop(0)  # Remove the oldest operation if history exceeds 20
-            
-        return jsonify({'question': expression, 'answer': result})
+        # Store operation in history database
+        cursor.execute('INSERT INTO history (operation, result) VALUES (?, ?)', (operation, result))
+        conn.commit()
+        
+        return jsonify({'question': operation, 'answer': result})
     except Exception as e:
         return jsonify({'error': str(e)})
 
 @app.route('/history', methods=['GET'])
 def history():
-    return jsonify(operations_history)
+    try:
+        cursor.execute('SELECT operation, result FROM history ORDER BY id DESC LIMIT 20')
+        history_data = [{'question': row[0], 'answer': row[1]} for row in cursor.fetchall()]
+        return jsonify({'history': history_data})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+@app.teardown_appcontext
+def close_connection(exception):
+    conn.close()
 
 if _name_ == '_main_':
     app.run(host='0.0.0.0', port=5000)
